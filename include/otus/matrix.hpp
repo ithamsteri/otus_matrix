4a7a9064251829bfbb9487906b6559566e3eb686
Alexander Yashkin <alex.aspirine@gmail.com>
2018-09-01 17:54:15 +0300

    Add the main part of the matrix

--- include/otus/matrix.hpp
+++ include/otus/matrix.hpp
@@ -1,15 +1,112 @@
+//
+//    +-+-+-+-+
+//    |O|T|U|S|
+//  +-+-+-+-+-+-+
+//  |M|A|T|R|I|X|
+//  +-+-+-+-+-+-+
+//
+// Filename: matrix.hpp
+// Require:  C++14 Standart
+//
+
 #ifndef OTUS_MATRIX_HPP
 #define OTUS_MATRIX_HPP
 
 #include <cstddef>
+#include <map>
+#include <tuple>
+#include <type_traits>
 
 namespace otus {
 
+/// Class of the dimensionless matrix.
+template <typename T, T DefaultValue, size_t Dimension = 2>
 class Matrix {
+  private:
+    static_assert(Dimension >= 2, "The dimension of the matrix must be equal or greater than 2");
+
+    template <typename TupleType, unsigned N, typename... Types>
+    struct generate_tuple_type {
+        using type = typename generate_tuple_type<TupleType, N - 1, TupleType, Types...>::type;
+    };
+
+    template <typename TupleType, typename... Types>
+    struct generate_tuple_type<TupleType, 0, Types...> {
+        using type = std::tuple<Types...>;
+    };
+
+    /// Class Layout provide access to other Layouts in the matrix.
+    template <size_t N, typename... Types>
+    class Layout;
+    /// Class Layout with parameter N = 1 for access to element of the matrix.
+    template <typename... Types>
+    class Layout<0, Types...>;
+
+    using Contanter = std::map<typename generate_tuple_type<size_t, Dimension>::type, T>;
+    using NextLayout = Layout<Dimension - 1, size_t>;
+
+    Contanter elements_;
+    const T defaultValue_{DefaultValue};
+
   public:
+    auto operator[](size_t idx) { return NextLayout(idx, elements_); }
+    const auto operator[](size_t idx) const { return NextLayout(idx, elements_); }
+
     /// Get real count of elements in matrix
     /// @return count of elements
-    size_t size() const noexcept { return 0; }
+    size_t size() const noexcept { return elements_.size(); }
+
+    /// Clears the mapped matrix.
+    void clear() { elements_.clear(); }
+};
+
+template <typename T, T DefaultValue, size_t Dimension>
+template <size_t N, typename... Types>
+class Matrix<T, DefaultValue, Dimension>::Layout {
+    using NextLayout = Layout<N - 1, Types..., size_t>;
+
+    const Matrix::Contanter &elements_;
+    std::tuple<Types...> tuple_;
+
+  public:
+    Layout(std::tuple<Types...> tuple, const Matrix::Contanter &elements)
+        : elements_{elements}, tuple_{tuple} {}
+
+    auto operator[](size_t idx) {
+        return NextLayout(std::tuple_cat(tuple_, std::tie(idx)), elements_);
+    }
+    const auto operator[](size_t idx) const {
+        return NextLayout(std::tuple_cat(tuple_, std::tie(idx)), elements_);
+    }
+};
+
+template <typename T, T DefaultValue, size_t Dimension>
+template <typename... Types>
+class Matrix<T, DefaultValue, Dimension>::Layout<0, Types...> {
+    const Matrix::Contanter &elements_;
+    std::tuple<Types...> tuple_;
+    T value_;
+
+  public:
+    Layout(std::tuple<Types...> tuple, const Matrix::Contanter &elements)
+        : elements_{elements}, tuple_{tuple} {
+        auto iter = elements_.find(tuple_);
+        value_ = (iter != elements_.cend()) ? iter->second : DefaultValue;
+    }
+
+    auto operator=(const T &value) {
+        if (value != DefaultValue) {
+            const_cast<Matrix::Contanter &>(elements_)[tuple_] = value;
+        } else {
+            auto iter = elements_.find(tuple_);
+            if (iter != elements_.cend())
+                const_cast<Matrix::Contanter &>(elements_).erase(iter);
+        }
+        value_ = value;
+        return *this;
+    }
+
+    operator const T &() const noexcept { return value_; }
 };
 
 } // namespace otus
--- tests/Matrix.test.cpp
+++ tests/Matrix.test.cpp
@@ -1,10 +1,72 @@
-#define CATCH_CONFIG_MAIN
-
 #include <catch2/catch.hpp>
 #include <otus/matrix.hpp>
 
-TEST_CASE("Matrix created empty", "[matrix]") {
-    otus::Matrix matrix;
+SCENARIO("2D Matrix operations", "[matrix][2D]") {
+    GIVEN("A Matrix with some elements") {
+        constexpr int DEFAULT_VALUE = -1;
+        otus::Matrix<int, DEFAULT_VALUE> matrix;
 
-    REQUIRE(matrix.size() == 0);
+        matrix[14][68] = 52;
+        matrix[139][1] = 871;
+        matrix[71][89] = 51;
+
+        REQUIRE(matrix[14][68] == 52);
+        REQUIRE(matrix[139][1] == 871);
+        REQUIRE(matrix[71][89] == 51);
+        REQUIRE(matrix.size() == 3);
+
+        const auto start_size = matrix.size();
+
+        WHEN("get any empty element") {
+            auto element = matrix[26][8];
+            THEN("empty element equal default value and no change size") {
+                REQUIRE(element == DEFAULT_VALUE);
+                REQUIRE(matrix.size() == start_size);
+            }
+        }
+
+        WHEN("clear matrix") {
+            matrix.clear();
+            THEN("delete all elements in matrix") {
+                REQUIRE(matrix[14][68] == DEFAULT_VALUE);
+                REQUIRE(matrix[139][1] == DEFAULT_VALUE);
+                REQUIRE(matrix[71][89] == DEFAULT_VALUE);
+                REQUIRE(matrix.size() == 0);
+            }
+        }
+
+        WHEN("assign value for empty element") {
+            matrix[100][100] = 314;
+            THEN("add new element and increment size") {
+                REQUIRE(matrix[100][100] == 314);
+                REQUIRE(matrix.size() == start_size + 1);
+            }
+        }
+
+        WHEN("modify an added element") {
+            matrix[139][1] = 178;
+            THEN("change value and don't change size") {
+                REQUIRE(matrix[139][1] == 178);
+                REQUIRE(matrix.size() == start_size);
+            }
+        }
+
+        WHEN("assign default value for an added element") {
+            int value = matrix[14][68];
+            matrix[14][68] = DEFAULT_VALUE;
+            THEN("delete element from matrix, get value before not change and decrement size") {
+                REQUIRE(value != DEFAULT_VALUE);
+                REQUIRE(matrix[14][68] == DEFAULT_VALUE);
+                REQUIRE(matrix.size() == start_size - 1);
+            }
+        }
+
+        WHEN("use the canonical assignment operator for empty element") {
+            ((matrix[51][47] = 23) = 161) = 78;
+            THEN("add new one element to matrix") {
+                REQUIRE(matrix[51][47] == 78);
+                REQUIRE(matrix.size() == start_size + 1);
+            }
+        }
+    }
 }
