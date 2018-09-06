47bbe6168a496f16520d6169971607328acc9af7
Alexander Yashkin <alex.aspirine@gmail.com>
2018-09-06 21:51:13 +0300

    Delete files for review

--- tests/Main.test.cpp
+++
@@ -1,7 +0,0 @@
-#define CATCH_CONFIG_MAIN
-
-#include <catch2/catch.hpp>
-
-// This file contain only main function of Catch2 library for fast compile.
-// https://github.com/catchorg/Catch2/blob/master/docs/tutorial.md#scaling-up
-// https://github.com/catchorg/Catch2/blob/master/docs/slow-compiles.md#top
